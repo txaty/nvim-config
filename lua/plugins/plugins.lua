a3e7d9597d15d669ea2689860182807ea4dc3c2a
Sam ツ <txaty@proton.me>
2025-01-07 19:35:00 +0800

    feat: Update theme

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -9,67 +9,15 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --   priority = 1000,
-  --   config = function()
-  --     require("gruvbox").setup({
-  --       transparent_mode = true, -- Enable transparent background
-  --     })
-  --     vim.cmd("colorscheme gruvbox")
-  --   end,
-  -- },
-  --
-  -- add tokyonight
-  -- { "folke/tokyonight.nvim" },
-  -- { "catppuccin/nvim", name = "catppuccin" },
-  -- {
-  --   "Shatur/neovim-ayu",
-  --   config = function ()
-  --     require('ayu').setup({
-  --       mirage = false,
-  --       terminal = true,
-  --       overrides = {},
-  --     })
-  --     require('ayu').colorscheme()
-  --   end,
-  -- },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("cyberdream").setup({
-        cache = true,
-        transparent = true,
-        italic_comments = true,
-        hide_fillchars = false,
-        borderless_telescope = true,
-        terminal_colors = true,
-        theme = {
-          variant = "auto",
-          saturation = 1,
-        },
-        extensions = {
-          telescope = true,
-          notify = true,
-          mini = true,
-        },
-      })
-      vim.cmd("colorscheme cyberdream")
-    end,
-  },
-
+  
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "gruvbox",
+      colorscheme = "gruvbox",
       -- colorscheme = "catppuccin",
       -- colorscheme = "ayu",
-      colorsheme = "cyberdream",
+      -- colorsheme = "cyberdream",
       },
   },
 
