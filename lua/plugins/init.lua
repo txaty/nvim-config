44d6c1b158c80c4a0b6a1be44c405f23db8cd9fc
Sam ツ <txaty@proton.me>
2025-01-30 11:16:08 +0800

    feat: Config for Rust development

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,10 @@ return {
     opts = require "configs.conform",
   },
 
+  {
+    "williamboman/mason.nvim"
+  },
+
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
@@ -13,6 +17,49 @@ return {
     end,
   },
 
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+			local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+		end,
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function()
+			require("dapui").setup()
+		end,
+  },
+
+  {
+    "saecki/crates.nvim",
+    ft = {"toml"},
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true
+          },
+        },
+      }
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+    end
+  },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
