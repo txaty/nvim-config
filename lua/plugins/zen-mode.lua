5f7913fe2cda9098f699ce63a281645f7f75fca2
Sam ツ <txaty@proton.me>
2026-01-30 21:22:58 +0800

    feat(plugins): add zen mode and twilight for distraction-free coding
    Add zen-mode.nvim with twilight.nvim for focused editing. Toggled via
    <leader>uz, dims inactive code and hides UI chrome (statusline, numbers,
    signcolumn). Includes txaty theme highlight groups.

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    keys = {
+      { "<leader>uz", "<cmd>ZenMode<cr>", desc = "UI: Toggle zen mode" },
+    },
+    opts = {
+      window = {
+        backdrop = 0.95,
+        width = 120,
+        height = 1,
+        options = {
+          signcolumn = "no",
+          number = false,
+          relativenumber = false,
+          cursorline = false,
+          cursorcolumn = false,
+          foldcolumn = "0",
+          list = false,
+        },
+      },
+      plugins = {
+        options = {
+          enabled = true,
+          ruler = false,
+          showcmd = false,
+          laststatus = 0,
+        },
+        twilight = { enabled = true },
+        gitsigns = { enabled = false },
+        tmux = { enabled = true },
+      },
+    },
+  },
+  {
+    "folke/twilight.nvim",
+    cmd = "Twilight",
+    opts = {
+      dimming = {
+        alpha = 0.25,
+        inactive = false,
+      },
+      context = 10,
+      treesitter = true,
+    },
+  },
+}
